ae4ea25ad1f017774515a1b7223447786f37fa5a
Steven Hermans <s.hermans@maxxton.com>
2016-05-20 14:24:24 +0200

    Add gradle plugin

--- src/main/groovy/com/maxxton/gradle/doc/MaxxtonDocumentationPlugin.groovy
+++ src/main/groovy/com/maxxton/gradle/doc/MaxxtonDocumentationPlugin.groovy
@@ -11,7 +11,7 @@ class MaxxtonDocumentationPlugin implements Plugin<Project>{
         
         project.task('extractSpringDoclet') <<{
             File tmpDir = new File("$project.buildDir/tmp")
-            File jarFile = new File(tmpDir, "spring-doclet-all-0.4.jar")
+            File jarFile = new File(tmpDir, "spring-doclet-all.jar")
             if(jarFile.exists()){
                 return;
             }
@@ -21,9 +21,9 @@ class MaxxtonDocumentationPlugin implements Plugin<Project>{
                 tmpDir.mkdirs()
                 jarFile.delete()
             
-                inputStream = MaxxtonDocumentationPlugin.class.getResourceAsStream("/spring-doclet-all-0.4.jar")
+                inputStream = MaxxtonDocumentationPlugin.class.getResourceAsStream("/spring-doclet-all.jar")
                 if(inputStream == null){
-                    throw new NullPointerException("Could not find '/spring-doclet-all-0.4.jar' in resources")
+                    throw new NullPointerException("Could not find '/spring-doclet-all.jar' in resources")
                 }
                 fileOut = new FileOutputStream(jarFile)
                 byte[] buffer = new byte[1024]
@@ -43,16 +43,18 @@ class MaxxtonDocumentationPlugin implements Plugin<Project>{
             }
         }
         
+		// deprecated
         project.task('maxxtonDocSpring', type: Javadoc, dependsOn: ['maxxtonDocJxr', 'extractSpringDoclet'], group: 'maxxton') {
             title = ""
             source = project.sourceSets.main.allJava
             classpath = project.sourceSets.main.compileClasspath
             destinationDir = project.reporting.file("springdoc")
-            options.docletpath = [new File("$project.buildDir/tmp/spring-doclet-all-0.4.jar")]
+            options.docletpath = [new File("$project.buildDir/tmp/spring-doclet-all.jar")]
             options.doclet = 'org.springdoclet.SpringDoclet'
             options.addStringOption("linkpath", "../jxr/");
         }
         
+		// deprecated
         project.task('maxxtonDocJavadoc', type: Javadoc, group: 'maxxton'){
             title = ""
             source = project.sourceSets.main.allJava
@@ -60,12 +62,42 @@ class MaxxtonDocumentationPlugin implements Plugin<Project>{
             classpath = project.sourceSets.main.compileClasspath
         }
         
+		// deprecated
         project.task('maxxtonDocJxr', type: Copy, dependsOn: ['jxr'], group: 'maxxton'){
             from new File(project.buildDir, 'jxr')
             into project.reporting.file("jxr")
         }
         
+		// deprecated
         project.task('maxxtonDoc', type:Zip, dependsOn: ['maxxtonDocSpring', 'maxxtonDocJavadoc', 'maxxtonDocJxr'], group: 'maxxton'){
+            from project.reporting.file("./")
+            baseName = "maxxton-doc"
+        }
+		
+        
+        project.task('buildApidoc', type: Javadoc, dependsOn: ['buildJxrDoc', 'extractSpringDoclet'], group: 'documentation') {
+            title = ""
+            source = project.sourceSets.main.allJava
+            classpath = project.sourceSets.main.compileClasspath
+            destinationDir = project.reporting.file("springdoc")
+            options.docletpath = [new File("$project.buildDir/tmp/spring-doclet-all.jar")]
+            options.doclet = 'org.springdoclet.SpringDoclet'
+            options.addStringOption("linkpath", "../jxr/");
+        }
+        
+        project.task('buildJavadoc', type: Javadoc, group: 'documentation'){
+            title = ""
+            source = project.sourceSets.main.allJava
+            destinationDir = project.reporting.file("javadoc")
+            classpath = project.sourceSets.main.compileClasspath
+        }
+        
+        project.task('buildJxrDoc', type: Copy, dependsOn: ['jxr'], group: 'documentation'){
+            from new File(project.buildDir, 'jxr')
+            into project.reporting.file("jxr")
+        }
+        
+        project.task('buildDoc', type:Zip, dependsOn: ['buildApidoc', 'buildJavadoc', 'buildJxrDoc'], group: 'documentation'){
             from project.reporting.file("./")
             baseName = "maxxton-doc"
         }
